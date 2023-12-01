ff5c881ce99e8d64bc18865c3700801a406a516d
Albert Salavatov <albert_sa@mail.ru>
2023-12-01 21:57:07 +0500

    add_02_refactoring

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,8 +1,14 @@
 const modals = () => {
-  function bindModal(triggersSelector, modalSelector, closeSelector) {
+  const bindModal = (triggersSelector, modalSelector, closeSelector) => {
     const triggers = document.querySelectorAll(triggersSelector);
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
+
+    const closeModal = () => {
+      modal.style.display = 'none';
+      document.body.style.overflow = '';
+      //document.body.classList.remove('modal-open')
+    };
 
     triggers.forEach((trigger) => {
       trigger.addEventListener('click', (e) => {
@@ -11,35 +17,37 @@ const modals = () => {
         }
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
-        // document.body.classList.add('modal-open');
+        //document.body.classList.add('modal-open');
       });
     });
 
     close.addEventListener('click', () => {
-      modal.style.display = 'none';
-      document.body.style.overflow = '';
-      //   document.body.classList.remove('modal-open');
+      closeModal();
     });
 
     modal.addEventListener('click', (e) => {
       if (e.target === modal) {
-        modal.style.display = 'none';
-        document.body.style.overflow = '';
-        // document.body.classList.remove('modal-open');
+        closeModal();
       }
     });
-  }
 
-  function showModalByTime(selector, time) {
-    setTimeout(function () {
+    document.addEventListener('keydown', (e) => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    });
+  };
+
+  const showModalByTime = (selector, time) => {
+    setTimeout(() => {
       document.querySelector(selector).style.display = 'block';
       document.body.style.overflow = 'hidden';
     }, time);
-  }
+  };
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
-  showModalByTime('.popup', 4000);
+  showModalByTime('.popup', 2000);
 };
 
 export default modals;
