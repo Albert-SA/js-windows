f5fa959caeb3eba61b4e4b32b29638e713bb4441
Albert Salavatov <albert_sa@mail.ru>
2023-12-03 13:49:51 +0500

    03_add_forms

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,3 +1,14 @@
-const forms = () => {};
+const forms = () => {
+  const formsSec = document.querySelectorAll('form');
+  const inputs = document.querySelectorAll('input');
+
+  const message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся...',
+    failure: 'Что-то пошло не так...',
+  };
+
+  formsSec.forEach((form) => {});
+};
 
 export default forms;
