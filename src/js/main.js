4740f66141ff37e459d962a296dadc8911e091b7
Albert Salavatov <albert_sa@mail.ru>
2023-12-04 22:10:10 +0500

    add_forms_post

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import './slider';
 import modals from './modules/modals';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
   modals();
+  forms();
 });
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,5 +1,5 @@
 const forms = () => {
-  const formsSec = document.querySelectorAll('form');
+  const formElems = document.querySelectorAll('form');
   const inputs = document.querySelectorAll('input');
 
   const message = {
@@ -8,7 +8,52 @@ const forms = () => {
     failure: 'Что-то пошло не так...',
   };
 
-  formsSec.forEach((form) => {});
+  const postData = async (url, data) => {
+    document.querySelector('.status').textContent = message.loading; // textContent == innerHtml
+    let res = await fetch(url, {
+      // fetch - асинхронный, поэтому async/await
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    }); //запрос POST
+
+    return await res.text(); //promise
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((input) => {
+      input.value = '';
+    });
+  };
+  formElems.forEach((form) => {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      let statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      form.appendChild(statusMessage);
+
+      const formData = new FormData(form); //тело запроса
+      const jsonData = JSON.stringify(Object.fromEntries(formData.entries()));
+
+      postData('https://simple-server-cumz.onrender.com/api/data', jsonData)
+        .then((res) => {
+          console.log(res);
+          statusMessage.textContent = message.success;
+        })
+        .catch(() => {
+          statusMessage.textContent = message.failure;
+        })
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
+        });
+    });
+  });
 };
 
 export default forms;
