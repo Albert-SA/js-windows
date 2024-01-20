421e0fdab365978ac68ee38d5d56dabfa0b4b0ea
Albert-SA <albert_sa@mail.ru>
2024-01-20 23:18:20 +0500

    27

--- src/js/main.js
+++ src/js/main.js
@@ -34,6 +34,6 @@ window.addEventListener('DOMContentLoaded', () => {
     display: 'inline-block',
   });
   forms(modalState);
-  timer('.container1', '2024-12-21');
+  timer('.container1', '2024-12-27');
   images();
 });
